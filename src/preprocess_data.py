b3b70034b0d92a5d53c16de53a3907e2edc9c00f
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-01-14 11:46:50 -0500

    [#5] moved preprocess_text() to PreprocessDataset class.
    Moved preprocess_text() to class.

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -26,7 +26,7 @@ from src.download_data import check_language
 from typing import List
 
 
-class PreprocessText(object):
+class PreprocessDataset(object):
     """"""
 
     def __init__(self, language: str, n_max_words_per_text: int) -> None:
@@ -163,106 +163,94 @@ class PreprocessText(object):
         # Replace consecutive whitespace characters with a single space.
         return " ".join(text.split())
 
+    def preprocess_text(self, text: str, update_word_count: bool) -> str:
+        """Preprocesses text to remove unwanted characters from it.
 
-def preprocess_text(
-    text: str, language: str, n_max_words_per_text: int, update_word_count: bool
-) -> str:
-    """Preprocesses text to remove unwanted characters from it.
+        Preprocesses text to remove unwanted characters from it.
 
-    Preprocesses text to remove unwanted characters from it.
+        Args:
+            text: A string for the text which needs to be processed.
+            update_unique_words: A boolean value for updating the word count.
 
-    Args:
-        text: A string for the text which needs to be processed.
-        language: A string for the name of the language the dataset belongs to.
-        n_max_words_per_text: An integer for the maximum number of words allowed in a text.
-        update_unique_words: A boolean value for updating the word count.
+        Returns:
+            A string for processed version of input text.
+        """
+        # Asserts type & values of the arguments.
+        assert isinstance(text, str), "Variable text should be of type 'str'."
+        assert isinstance(
+            update_word_count, bool
+        ), "Variable update_word_count should be of type 'bool'."
 
-    Returns:
-        A string for processed version of input text.
-    """
-    # Asserts type & values of the arguments.
-    assert isinstance(text, str), "Variable text should be of type 'str'."
-    assert isinstance(language, str), "Variable language should be of type 'str'."
-    assert language in [
-        "en",
-        "es",
-        "fr",
-        "de",
-    ], "Variable language should have value as 'en', 'es', 'fr', or 'de'."
-    assert isinstance(
-        n_max_words_per_text, int
-    ), "Variable n_max_words_per_text should be of type 'int'."
+        # Removes HTML markup components from text provided as input.
+        text = self.remove_html_markup(text)
 
-    # Removes HTML markup components from text provided as input.
-    text = remove_html_markup(text)
+        # Converts text to lowercase characters, & strip leading & trailing whitespace.
+        text = text.lower().strip()
 
-    # Converts text to lowercase characters, & strip leading & trailing whitespace.
-    text = text.lower().strip()
+        # Replaces unwanted characters in text.
+        text = text.replace("##at##-##at##", "-")
+        text = text.replace("&apos;", "'")
+        text = text.replace("&quot;", '"')
+        text = text.replace("&#91;", "")
+        text = text.replace("&#93;", "")
+        text = text.replace("&#124;", "")
+        text = text.replace('"', ' " ')
 
-    # Replaces unwanted characters in text.
-    text = text.replace("##at##-##at##", "-")
-    text = text.replace("&apos;", "'")
-    text = text.replace("&quot;", '"')
-    text = text.replace("&#91;", "")
-    text = text.replace("&#93;", "")
-    text = text.replace("&#124;", "")
-    text = text.replace('"', ' " ')
+        # Based on name of the language, removes characters from text.
+        if self.language == "en":
+            text = "".join(
+                id_0
+                for id_0 in unicodedata.normalize("NFKD", str(text))
+                if unicodedata.category(id_0) != "Mn"
+            )
+            text = re.sub(r"[^-!$&(),./%0-9:;?a-z€'\"]+", " ", text)
 
-    # Based on name of the language, removes characters from text.
-    if language == "en":
-        text = "".join(
-            id_0
-            for id_0 in unicodedata.normalize("NFKD", str(text))
-            if unicodedata.category(id_0) != "Mn"
-        )
-        text = re.sub(r"[^-!$&(),./%0-9:;?a-z€'\"]+", " ", text)
+        elif self.language == "es":
+            text = re.sub(r"[^-!$&(),./%0-9:;?ÁÉÍÓÚÑÜáéíóúñü¿¡a-z€'\"]+", " ", text)
 
-    elif language == "es":
-        text = re.sub(r"[^-!$&(),./%0-9:;?ÁÉÍÓÚÑÜáéíóúñü¿¡a-z€'\"]+", " ", text)
+        elif self.language == "fr":
+            text = re.sub(r"[^-!$&(),./%0-9:;?!çàâæéèêëîïôöûüù'€\"*]+", " ", text)
 
-    elif language == "fr":
-        text = re.sub(r"[^-!$&(),./%0-9:;?!çàâæéèêëîïôöûüù'€\"*]+", " ", text)
+        elif self.language == "de":
+            text = re.sub(r"[^-!$&(),./%0-9:;?!äöüßœáéíóúñüa-z'€\"*]+", " ", text)
 
-    elif language == "de":
-        text = re.sub(r"[^-!$&(),./%0-9:;?!äöüßœáéíóúñüa-z'€\"*]+", " ", text)
+        # Collapses repeated punctuation.
+        text = re.sub(r"\.{2,}", ".", text)
 
-    # Collapses repeated punctuation.
-    text = re.sub(r"\.{2,}", ".", text)
+        # Fix ordinal numbers (e.g., 1st, 2nd)
+        text = re.sub(r"(\d)th", r"\1 th", text, flags=re.I)
+        text = re.sub(r"(\d)st", r"\1 st", text, flags=re.I)
+        text = re.sub(r"(\d)rd", r"\1 rd", text, flags=re.I)
+        text = re.sub(r"(\d)nd", r"\1 nd", text, flags=re.I)
 
-    # Fix ordinal numbers (e.g., 1st, 2nd)
-    text = re.sub(r"(\d)th", r"\1 th", text, flags=re.I)
-    text = re.sub(r"(\d)st", r"\1 st", text, flags=re.I)
-    text = re.sub(r"(\d)rd", r"\1 rd", text, flags=re.I)
-    text = re.sub(r"(\d)nd", r"\1 nd", text, flags=re.I)
+        # Separate punctuation with spaces
+        punctuations = "-!$&(),./%:;?!çàâæéèêëîïôöûüù'€\"*"
+        for character in punctuations:
+            text = text.replace(character, " " + character + " ")
 
-    # Separate punctuation with spaces
-    punctuations = "-!$&(),./%:;?!çàâæéèêëîïôöûüù'€\"*"
-    for character in punctuations:
-        text = text.replace(character, " " + character + " ")
+        # Splits text into list of words as strings.
+        text_words = text.split(" ")
 
-    # Splits text into list of words as strings.
-    text_words = text.split(" ")
+        # If no. of words in current text is more than maximum limit, then text is ignored.
+        if len(text_words) > self.n_max_words_per_text:
+            return ""
 
-    # If no. of words in current text is more than maximum limit, then text is ignored.
-    if len(text_words) > n_max_words_per_text:
-        return ""
+        # Iterates across words in text.
+        filtered_words = list()
+        for word in text_words:
+            # If word is not empty, then it is appended to list.
+            if word != "":
+                filtered_words.append(word)
 
-    # Iterates across words in text.
-    filtered_words = list()
-    for word in text_words:
-        # If word is not empty, then it is appended to list.
-        if word != "":
-            filtered_words.append(word)
+                # Unique word count is updated for current word.
+                if update_word_count:
+                    unique_words_count[self.language][word] = 1 + unique_words_count[
+                        self.language
+                    ].get(word, 0)
 
-            # Unique word count is updated for current word.
-            if update_word_count:
-                unique_words_count[language][word] = 1 + unique_words_count[
-                    language
-                ].get(word, 0)
-
-    # Converts of list of filtered words into a single string.
-    filtered_text = " ".join(filtered_words)
-    return filtered_text
+        # Converts of list of filtered words into a single string.
+        filtered_text = " ".join(filtered_words)
+        return filtered_text
 
 
 def preprocess_tatoeba_dataset(
